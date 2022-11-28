50dc1260f68f94c4887fe266ef829c0e49017138
davijaf <davijose@gmail.com>
2022-11-27 23:49:53 -0300

    segundo commit

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # JQueryAjax
-JQuery e Ajax app
+
+Consulta CEP utilizando JQuery, Ajax e Bootstrap
 
 
-Requisiçao AJAX utilizando a biblioteca JQuery
+Requisiçao AJAX utilizando a biblioteca JQuery + Bootstrap
---
+++ js/main.js
@@ -0,0 +1,26 @@
+alert("Funcionou!")
+function consultaCEP(){
+    $(".barra-progresso").show();
+    var cep = document.getElementById("CEP").value;
+    var url= "https://viacep.com.br/ws/"+cep+"/json";
+    console.log(url);
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#tituloCEP").html("CEP "+response.cep);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $(".cep").show();
+            $(".barra-progresso").hide();
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            //document.getElementById("bairro").innerHTML = response.bairro;
+            //document.getElementById("localidade").innerHTML = response.localidade;
+            //document.getElementById("uf").innerHTML = response.uf;
+            //alert(response.logradouro)
+        }
+    })
+};
